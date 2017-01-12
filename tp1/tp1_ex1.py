9d6d68419bd164e88b4a978feb790deaaca2f281
pierrick <deshayes.pierrick@hotmail.fr>
2017-01-12 16:36:01 +0100

    update

--- tp1/tp1_ex1.py
+++ tp1/tp1_ex1.py
@@ -1,62 +1,56 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from scipy.spatial import distance
+
 data=datasets.load_iris()
 
-# nombre de classes : 3 ('setosa', 'versicolor', 'virginica')
-# n = 150 (50 / classe)
-# 4 attributs numériques (sepal length, sepal width, petal length, petal width)
+n=len(data.data)
+nb_class=len(data.target_names)
+d=len(data.feature_names)
 
-print(data.data)
-print(data.feature_names)
-print(data.target)
-print(data.target_names)
+print('nombre d\'exemples :',n)
+print('nombre de classes :',nb_class)
+print('nombre d\'attributs :',d)
+print('attributs :',' '.join(data.feature_names))
 
-dapp = dict()
-dapp["array"] = list()
-dapp["class"] = list()
+def split(X, C):
+    d_app=list()
+    c_app=list()
+    d_test=list()
+    c_test=list()
+    for i in range(len(X)):
+        if (i % 2 == 0):
+            d_app.append(X[i])
+            c_app.append(C[i])
+        else:
+            d_test.append(X[i])
+            c_test.append(C[i])
+    return d_app, c_app, d_test, c_test
 
-dtest = dict()
-dtest["array"] = list()
-dtest["class"] = list()
+def kppv(x, d_app, c_app, k):
+    distances=list()
+    classes=list()
+    for i in range(len(d_app)):
+        distances.append(distance.euclidean(x, d_app[i])) #distance entre x et toutes les données de l'ensemble d'apprentissage 
+    sorted_list=np.argsort(distances) #liste croissante des indices des données les plus proches
+    for i in range(len(d_app)):
+        classes.append(c_app[sorted_list[i]]) #classes des k plus proches voisins
+    return np.argmax(np.bincount(classes[:k])) #retourne la classe dominante
+    
+d_app, c_app, d_test, c_test = split(data.data, data.target)
 
-for i in range(len(data.data)):
-    if (i % 2 == 0):
-        dapp.get("array").append(data.data[i])
-        dapp.get("class").append(data.target[i])
-    else:
-        dtest.get("array").append(data.data[i])
-        dtest.get("class").append(data.target[i])
-
-
-def kppv(k, x):
-    l = list()
-    for i in range(len(dapp["array"])):
-        l.append(distance.euclidean(x, dapp["array"][i])) #distance entre x et toutes les données du dataset 
-    sorted_list = numpy.argsort(l) #liste croissante des indices des données les plus proches
-    classes = list()
-    for i in range(len(dapp["array"])):
-        classes.append(dtest["class"][sorted_list[i]]) #classe des k plus proches voisins
-    return election(classes[:k]) #retourne la classe dominante
-
-
-def election(int_list):
-    return numpy.argmax(numpy.bincount(int_list))
-
-
-y = list()
+rates=list()
 for k in range(1, 100):    
     good = 0
-    for i in range(len(dtest["array"])):
-        p = kppv(k, dtest["array"][i]) #classe prédite
-        r = dtest["class"][i] #classe réelle
+    for i in range(len(d_test)):
+        p = kppv(d_test[i], d_app, c_app, k) #classe prédite
+        r = c_test[i] #classe réelle
         if p == r:
             good += 1
-        rate = good / len(dtest["array"]) * 100
-    y.append(rate)
+        rate = good / len(d_test) * 100
+    rates.append(rate)
                 
     print('k = ',k, ' ', rate,'%')
 
-plt.plot(y)
-
+plt.plot(rates)
--- tp1/tp1_ex2.py
+++ tp1/tp1_ex2.py
@@ -5,114 +5,62 @@ Created on Fri Nov 25 10:59:07 2016
 @author: Pierrick
 """
 
-import numpy
-import random
+import numpy as np
 import matplotlib.pyplot as plt
 
-nbPoints = 100
-
-dataset = list()
-dapp = list()
-dtest = list()
-
-class Point:
-    
-    def __init__(self, x1, x2):
-        self.x1 = x1
-        self.x2 = x2
-        self.cl = 0
-        
-    def setClass(self):
-        if (-self.x1/2 + 0.75) <= self.x2:
-            self.cl = 1
+def datagen(nb_points):
+    dataset=list()
+    for i in range(nb_points):
+        p=[np.random.random() for i in range(2)]
+        if (-p[0]/2 + 0.75) <= p[1]:
+            p.append(1)
         else:
-            self.cl = -1
-            
-    def getClass(self):
-        return self.cl
-        
-    def toString(self):
-        return self.x1,self.x2,self.cl
-
-
-def datagen():
-    for i in range(nbPoints):
-        p = Point(numpy.random.random(), numpy.random.random())
-        p.setClass()
+            p.append(-1)
         dataset.append(p)
-#        print(p.toString())
+    return dataset
     
-
+def split(dataset, d_app_prop, nb_points):
+    return dataset[:int(d_app_prop*nb_points)], dataset[int(d_app_prop*nb_points):]
+    
+def ptrain(d_app):
+    theta=np.array([np.random.random() for i in range(3)])
+    i=0
+    while i < len(d_app):
+        xplus=np.array([d_app[i][0], d_app[i][1], 1])
+        if sign(np.vdot(theta, xplus)) == d_app[i][2]:
+            i+=1
+        else:
+            theta=theta + d_app[i][2]*xplus
+            i=0
+    return theta
+    
 def sign(x):
     if x >= 0:
         return 1
     return -1
-
- 
-def repartition():
-    for i in range(int(0.8*100)):
-        dapp.append(dataset.pop(random.randint(0, len(dataset)-1)))
-    for i in range(len(dataset)):
-        dtest.append(dataset[i])
-   
-
-def ptrain():
-    theta = numpy.array([numpy.random.random(),numpy.random.random(),numpy.random.random()])
-    i = 0
-    while i < len(dapp):
-        xplus = numpy.array([dapp[i].x1, dapp[i].x2, 1])
-        if sign(numpy.vdot(theta, xplus)) == dapp[i].cl:
-            i += 1
-        else:
-            theta = theta + dapp[i].cl*xplus
-            i = 0
-    return theta
-
-
-def ptest(x):
-    xplus = numpy.array([x.x1, x.x2, 1])
-    return sign(numpy.vdot(xplus, theta))
-
-datagen()
     
-xNeg = list()
-yNeg = list()
-xPos = list()
-yPos = list()
+def ptest(x, theta):
+    xplus=np.array([x[0], x[2], 1])
+    return sign(np.vdot(xplus, theta))
 
-for i in range(nbPoints):
-    if dataset[i].cl == -1:
-        xNeg.append(dataset[i].x1)
-        yNeg.append(dataset[i].x2)
-    else:
-        xPos.append(dataset[i].x1)
-        yPos.append(dataset[i].x2)
-
-plt.plot(xNeg,yNeg,"or")
-plt.plot(xPos,yPos,"ob")
-
-
-repartition()
-
-    
-theta = ptrain()
-
-w1 = theta[0]
-w2 = theta[1]
-b = theta[2]
-print(w1, w2, b)
-    
-plt.plot([(-w1*x - b) / w2 for x in range(2)], color="black")
+dataset=datagen(100)
+x_pos=[dataset[i][0] for i in range(100) if dataset[i][2]==1]
+y_pos=[dataset[i][1] for i in range(100) if dataset[i][2]==1]
+x_neg=[dataset[i][0] for i in range(100) if dataset[i][2]==-1]
+y_neg=[dataset[i][1] for i in range(100) if dataset[i][2]==-1]
+plt.plot(x_neg,y_neg,'or')
+plt.plot(x_pos,y_pos,'ob')
+d_app,d_test=split(dataset,0.8,100)
+theta=ptrain(d_app)
+plt.plot([(-theta[0]*x - theta[2]) / theta[1] for x in range(2)], color="black")
 
 total = 0
 for i in range(100):
-    cpt = 0
-    for p in dtest:
-        real = p.getClass()
-        pred = ptest(p)
-        if pred == real:
-            cpt += 1
-#        print("classe reelle : ", real, " ,classe prédite : ", pred)
-    total += cpt/len(dtest) * 100
-    
+    cpt=0
+    for p in d_test:
+        real=p[2]
+        pred=ptest(p,theta)
+        if pred==real:
+            cpt+=1
+    total += cpt/len(d_test)*100 
 print(total/100, "% de bonne classification")
